b4097f312e689a4ab005f6780b1694e67eea251b
Vincent van Hoef <vvanhoef@hotmail.com>
2022-06-07 09:44:32 +0200

    add qc

--- Results/gsea_script.R
+++ Results/gsea_script.R
@@ -7,9 +7,9 @@ library(enrichplot)
 library(WebGestaltR)
 library(dplyr)
 
-de_files <- list.files("../DESeq2/", pattern = ".xlsx", full.names = TRUE, recursive=TRUE)
+de_files <- list.files("./DESeq2/", pattern = ".xlsx", full.names = TRUE, recursive=TRUE)
 de_files <- lapply(de_files, read_excel)
-names(de_files) <- gsub("/.*", "", list.files("../DESeq2/", pattern = ".xlsx", full.names = FALSE, recursive=TRUE))
+names(de_files) <- gsub("/.*", "", list.files("./DESeq2/", pattern = ".xlsx", full.names = FALSE, recursive=TRUE))
 
 create_go <- function(df, con = x){
   
@@ -21,7 +21,7 @@ create_go <- function(df, con = x){
   x_tss_up <- x_tss %>% filter(log2FoldChange > 0) %>% pull(Entrez.ID) %>% unique
   x_tss_down <- x_tss %>% filter(log2FoldChange < 0) %>% pull(Entrez.ID) %>% unique
   
-  dir.create(paste0("../DESeq2/", con, "/Pathway_Enrichment/"), showWarnings = FALSE)
+  dir.create(paste0("./DESeq2/", con, "/Pathway_Enrichment/"), showWarnings = FALSE)
   
   WebGestaltR(enrichMethod = "ORA",
                     organism = "hsapiens",
@@ -31,7 +31,7 @@ create_go <- function(df, con = x){
                     interestGene = x_tss_up,
                     referenceGene = universe,
                     projectName = paste0(con, "_go_up"),
-                    outputDirectory = paste0("../DESeq2/", con, "/Pathway_Enrichment/")) 
+                    outputDirectory = paste0("./DESeq2/", con, "/Pathway_Enrichment/")) 
 
  WebGestaltR(enrichMethod = "ORA",
                    organism = "hsapiens",
@@ -41,13 +41,13 @@ create_go <- function(df, con = x){
                    interestGene = x_tss_down,
                    referenceGene = universe,
                    projectName = paste0(con, "_go_down"),
-                   outputDirectory = paste0("../DESeq2/", con, "/Pathway_Enrichment/")) 
+                   outputDirectory = paste0("./DESeq2/", con, "/Pathway_Enrichment/")) 
 
 # Modify command, this one seemed to work on MacOS...
-system(paste0("sed -i '' -e '/<header>/,/<\\/header>/d' ../DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_go_down/Report_", con, "_go_down.html"))
-system(paste0("sed -i '' -e '/<header>/,/<\\/header>/d' ../DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_go_up/Report_", con, "_go_up.html"))
-system(paste0("sed -i '' -e '/<footer/,/<\\/footer>/d' ../DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_go_down/Report_", con, "_go_down.html"))
-system(paste0("sed -i '' -e '/<footer/,/<\\/footer>/d' ../DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_go_up/Report_", con, "_go_up.html"))
+system(paste0("sed -i '' -e '/<header>/,/<\\/header>/d' ./DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_go_down/Report_", con, "_go_down.html"))
+system(paste0("sed -i '' -e '/<header>/,/<\\/header>/d' ./DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_go_up/Report_", con, "_go_up.html"))
+system(paste0("sed -i '' -e '/<footer/,/<\\/footer>/d' ./DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_go_down/Report_", con, "_go_down.html"))
+system(paste0("sed -i '' -e '/<footer/,/<\\/footer>/d' ./DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_go_up/Report_", con, "_go_up.html"))
 
 WebGestaltR(enrichMethod = "ORA",
             organism = "hsapiens",
@@ -57,7 +57,7 @@ WebGestaltR(enrichMethod = "ORA",
             interestGene = x_tss_up,
             referenceGene = universe,
             projectName = paste0(con, "_kegg_up"),
-            outputDirectory = paste0("../DESeq2/", con, "/Pathway_Enrichment/")) 
+            outputDirectory = paste0("./DESeq2/", con, "/Pathway_Enrichment/")) 
 
 WebGestaltR(enrichMethod = "ORA",
             organism = "hsapiens",
@@ -67,17 +67,17 @@ WebGestaltR(enrichMethod = "ORA",
             interestGene = x_tss_down,
             referenceGene = universe,
             projectName = paste0(con, "_kegg_down"),
-            outputDirectory = paste0("../DESeq2/", con, "/Pathway_Enrichment/")) 
+            outputDirectory = paste0("./DESeq2/", con, "/Pathway_Enrichment/")) 
 
 # Modify command, this one seemed to work on MacOS...
-system(paste0("sed -i '' -e '/<header>/,/<\\/header>/d' ../DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_kegg_down/Report_", con, "_kegg_down.html"))
-system(paste0("sed -i '' -e '/<header>/,/<\\/header>/d' ../DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_kegg_up/Report_", con, "_kegg_up.html"))
-system(paste0("sed -i '' -e '/<footer/,/<\\/footer>/d' ../DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_kegg_down/Report_", con, "_kegg_down.html"))
-system(paste0("sed -i '' -e '/<footer/,/<\\/footer>/d' ../DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_kegg_up/Report_", con, "_kegg_up.html"))
+system(paste0("sed -i '' -e '/<header>/,/<\\/header>/d' ./DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_kegg_down/Report_", con, "_kegg_down.html"))
+system(paste0("sed -i '' -e '/<header>/,/<\\/header>/d' ./DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_kegg_up/Report_", con, "_kegg_up.html"))
+system(paste0("sed -i '' -e '/<footer/,/<\\/footer>/d' ./DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_kegg_down/Report_", con, "_kegg_down.html"))
+system(paste0("sed -i '' -e '/<footer/,/<\\/footer>/d' ./DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_kegg_up/Report_", con, "_kegg_up.html"))
 
   } else {
   
-    dir.create(paste0("../DESeq2/", con, "/Pathway_Enrichment/"), showWarnings = FALSE)
+    dir.create(paste0("./DESeq2/", con, "/Pathway_Enrichment/"), showWarnings = FALSE)
     
     x_tss_gsea <- df %>% filter(abs(Distance.to.TSS) < 1000)
     ranks <- data.frame(genes = x_tss_gsea$Entrez.ID,
@@ -93,10 +93,10 @@ system(paste0("sed -i '' -e '/<footer/,/<\\/footer>/d' ../DESeq2/", con, "/Pathw
                 interestGene = ranks,
                 referenceGene = universe,
                 projectName = paste0(con, "_gsea"),
-                outputDirectory = paste0("../DESeq2/", con, "/Pathway_Enrichment"))    
+                outputDirectory = paste0("./DESeq2/", con, "/Pathway_Enrichment"))    
     
-    system(paste0("sed -i '' -e '/<header>/,/<\\/header>/d' ../DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_gsea/Report_", con, "_gsea.html"))
-    system(paste0("sed -i '' -e '/<footer/,/<\\/footer>/d' ../DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_gsea/Report_", con, "_gsea.html"))
+    system(paste0("sed -i '' -e '/<header>/,/<\\/header>/d' ./DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_gsea/Report_", con, "_gsea.html"))
+    system(paste0("sed -i '' -e '/<footer/,/<\\/footer>/d' ./DESeq2/", con, "/Pathway_Enrichment/Project_", con, "_gsea/Report_", con, "_gsea.html"))
     
 }
 }
